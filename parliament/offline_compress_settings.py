a0544c2ef99bba736b5bc65f80830a99773fc257
Mo Abdulhussain <abdulhu6@gmail.com>
2025-10-26 16:54:22 -0400

    Set COMPRESS_ROOT to frontend_bundles in settings
    Define COMPRESS_ROOT in both production (settings.py) and the offline
    compression settings so django-compressor can locate the prebuilt
    frontend bundles. Ensure os is imported in offline_compress_settings.

--- parliament/offline_compress_settings.py
+++ parliament/offline_compress_settings.py
@@ -1,8 +1,11 @@
 # A hack used to coax django-compressor to properly generate bundles
+import os
+
 from .default_settings import *
 
 DEBUG = False
 COMPRESS_ENABLED = True
 COMPRESS_OFFLINE = True
+COMPRESS_ROOT = os.path.join(os.path.dirname(PROJ_ROOT), 'frontend_bundles')
 
 SECRET_KEY = 'compression!'
