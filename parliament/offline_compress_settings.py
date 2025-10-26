00bad8361481fdf205a3d483b7c87b545433544b
Mo Abdulhussain <abdulhu6@gmail.com>
2025-10-26 17:04:28 -0400

    Import os and move STATIC_ROOT definition below DEBUG in offline_compress_settings

--- parliament/offline_compress_settings.py
+++ parliament/offline_compress_settings.py
@@ -1,7 +1,10 @@
 # A hack used to coax django-compressor to properly generate bundles
+import os
+
 from .default_settings import *
 
 DEBUG = False
+STATIC_ROOT = os.path.realpath(os.path.join(os.path.dirname(PROJ_ROOT), 'staticfiles'))
 COMPRESS_ENABLED = True
 COMPRESS_OFFLINE = True
 COMPRESS_ROOT = STATIC_ROOT
