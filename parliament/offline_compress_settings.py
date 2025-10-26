5e53a8df1ec71b36585ea66920178ed318d1d9ce
Mo Abdulhussain <abdulhu6@gmail.com>
2025-10-26 17:04:48 -0400

    Wrap STATIC_ROOT path construction across multiple lines for readability

--- parliament/offline_compress_settings.py
+++ parliament/offline_compress_settings.py
@@ -4,7 +4,8 @@ import os
 from .default_settings import *
 
 DEBUG = False
-STATIC_ROOT = os.path.realpath(os.path.join(os.path.dirname(PROJ_ROOT), 'staticfiles'))
+STATIC_ROOT = os.path.realpath(os.path.join(
+    os.path.dirname(PROJ_ROOT), 'staticfiles'))
 COMPRESS_ENABLED = True
 COMPRESS_OFFLINE = True
 COMPRESS_ROOT = STATIC_ROOT
