ad80b957b8ea9592aac4a2aaedda8b1a65803219
Mo Abdulhussain <abdulhu6@gmail.com>
2025-10-27 02:10:40 -0400

    test: disable ssl redirect and compression during smoke tests

--- parliament/politicians/tests.py
+++ parliament/politicians/tests.py
@@ -1,22 +1,28 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings
 
 from parliament.core.models import Politician
 
+
+@override_settings(SECURE_SSL_REDIRECT=False, COMPRESS_ENABLED=False, COMPRESS_OFFLINE=False)
 class SmokeTests(TestCase):
-    
+
     fixtures = ['parties', 'ridings', 'sessions', 'politicians']
-    
+
     def test_pages(self):
-        
+
         self.assertContains(self.client.get('/politicians/'), 'Current MPs')
-        
+
         assert self.client.get('/politicians/former/').status_code == 200
-        
-        self.assertContains(self.client.get('/politicians/hedy-fry/'), 'Vancouver Centre')
-        
-        assert self.client.get('/politicians/frank-mckenna/').status_code == 404
-        
+
+        self.assertContains(self.client.get(
+            '/politicians/hedy-fry/'), 'Vancouver Centre')
+
+        assert self.client.get(
+            '/politicians/frank-mckenna/').status_code == 404
+
         rona = Politician.objects.get_by_name('Rona Ambrose')
-        
-        self.assertContains(self.client.get('/politicians/%s/rss/statements/' % rona.id), 'Rona ')
-        self.assertContains(self.client.get('/politicians/%s/rss/activity/' % rona.id), 'Rona ')
+
+        self.assertContains(self.client.get(
+            '/politicians/%s/rss/statements/' % rona.id), 'Rona ')
+        self.assertContains(self.client.get(
+            '/politicians/%s/rss/activity/' % rona.id), 'Rona ')
