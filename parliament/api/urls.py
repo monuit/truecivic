3581b485a48763f0d11a8e6c250d847711301d64
Mo Abdulhussain <abdulhu6@gmail.com>
2025-10-27 02:10:32 -0400

    feat: add rag api endpoints for context and chunks retrieval

--- parliament/api/urls.py
+++ parliament/api/urls.py
@@ -1,8 +1,14 @@
 from django.urls import re_path
 
 from parliament.api.views import hansard, hansard_list
+from parliament.rag.api_views import KnowledgeChunkListView
+from parliament.rag.views import RagContextView
 
 urlpatterns = [
-    re_path(r'^hansards/(?P<hansard_id>\d+)/$', hansard, name='legacy_api_hansard'),
-    re_path(r'^hansards/$', hansard_list, name='legacy_api_hansard_list'),   
+    re_path(r'^hansards/(?P<hansard_id>\d+)/$',
+            hansard, name='legacy_api_hansard'),
+    re_path(r'^hansards/$', hansard_list, name='legacy_api_hansard_list'),
+    re_path(r'^rag/context/$', RagContextView.as_view(), name='rag_context'),
+    re_path(r'^rag/chunks/$', KnowledgeChunkListView.as_view(),
+            name='rag_chunk_list'),
 ]
