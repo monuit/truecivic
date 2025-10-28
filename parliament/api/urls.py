31ef97f10de4c05c1b8e0e9d964cca085a734eeb
Mo Abdulhussain <abdulhu6@gmail.com>
2025-10-28 02:22:45 -0400

    chore: add/modify parliament/api/urls.py

--- parliament/api/urls.py
+++ parliament/api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import re_path
+from django.urls import include, path, re_path
 
 from parliament.api.views import hansard, hansard_list
 from parliament.rag.api_views import KnowledgeChunkListView
@@ -8,6 +8,7 @@ urlpatterns = [
     re_path(r'^hansards/(?P<hansard_id>\d+)/$',
             hansard, name='legacy_api_hansard'),
     re_path(r'^hansards/$', hansard_list, name='legacy_api_hansard_list'),
+    path('v1/', include('parliament.api.v1.urls')),
     re_path(r'^rag/context/$', RagContextView.as_view(), name='rag_context'),
     re_path(r'^rag/chunks/$', KnowledgeChunkListView.as_view(),
             name='rag_chunk_list'),
