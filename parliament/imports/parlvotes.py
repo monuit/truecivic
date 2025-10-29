fb5a2a51ee782459059fdd5e514db5c4dc9e17d8
Mo Abdulhussain <abdulhu6@gmail.com>
2025-10-28 20:14:05 -0400

    Watermark vote imports

--- parliament/imports/parlvotes.py
+++ parliament/imports/parlvotes.py
@@ -4,10 +4,11 @@ from lxml import etree
 import requests
 
 from django.db import transaction
+from django.utils import timezone
 
 from parliament.bills.models import Bill, VoteQuestion, MemberVote
-from parliament.core.models import ElectedMember, Politician, Riding, Session
-from parliament.core import parsetools
+from parliament.core.models import ElectedMember, Politician, Session
+from parliament.orchestration.watermarks import get_watermark, update_watermark
 
 import logging
 logger = logging.getLogger(__name__)
@@ -17,6 +18,12 @@ VOTEDETAIL_URL = 'https://www.ourcommons.ca/members/en/votes/{parliamentnum}/{se
 
 @transaction.atomic
 def import_votes():
+    watermark = get_watermark("votes")
+    watermark_meta = dict(watermark.metadata or {})
+    latest_timestamp = watermark.timestamp
+    latest_token = watermark.token
+    latest_meta = dict(watermark_meta)
+
     votelisturl_en = VOTELIST_URL.format(lang='en')
     resp = requests.get(votelisturl_en)
     resp.raise_for_status()
@@ -26,7 +33,6 @@ def import_votes():
     resp = requests.get(votelisturl_fr)
     resp.raise_for_status()
     root_fr = etree.fromstring(resp.content)
-
     votelist = root.findall('Vote')
     for vote in votelist:
         votenumber = int(vote.findtext('DecisionDivisionNumber'))
@@ -34,15 +40,65 @@ def import_votes():
             parliamentnum=int(vote.findtext('ParliamentNumber')),
             sessnum=int(vote.findtext('SessionNumber'))
         )
-        if VoteQuestion.objects.filter(session=session, number=votenumber).count():
+
+        token_meta = {
+            'parliament': session.parliamentnum,
+            'session': session.sessnum,
+            'vote': votenumber,
+        }
+        token = f"{token_meta['parliament']}:{token_meta['session']}:{votenumber}"
+
+        event_dt = datetime.datetime.strptime(
+            vote.findtext('DecisionEventDateTime'), '%Y-%m-%dT%H:%M:%S'
+        )
+        if timezone.is_naive(event_dt):
+            event_dt = timezone.make_aware(event_dt, timezone=timezone.utc)
+        vote_date = event_dt.date()
+
+        skip_due_to_watermark = False
+        if watermark.timestamp:
+            if event_dt < watermark.timestamp:
+                skip_due_to_watermark = True
+            elif event_dt == watermark.timestamp:
+                same_parl = (
+                    token_meta['parliament'] == watermark_meta.get('parliament')
+                )
+                same_session = (
+                    token_meta['session'] == watermark_meta.get('session')
+                )
+                last_number = watermark_meta.get('vote')
+                if (
+                    same_parl
+                    and same_session
+                    and isinstance(last_number, int)
+                    and token_meta['vote'] <= last_number
+                ):
+                    skip_due_to_watermark = True
+
+        existing = VoteQuestion.objects.filter(
+            session=session, number=votenumber
+        ).exists()
+        if skip_due_to_watermark and existing:
             continue
+        if existing:
+            if (
+                latest_timestamp is None
+                or event_dt > latest_timestamp
+                or (
+                    event_dt == latest_timestamp
+                    and token_meta['vote'] > latest_meta.get('vote', -1)
+                )
+            ):
+                latest_timestamp = event_dt
+                latest_token = token
+                latest_meta = token_meta
+            continue
+
         print("Processing vote #%s" % votenumber)
-        date = vote.findtext('DecisionEventDateTime')
-        date = datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%S').date()
         votequestion = VoteQuestion(
             number=votenumber,
             session=session,
-            date=date,
+            date=vote_date,
             yea_total=int(vote.findtext('DecisionDivisionNumberOfYeas')),
             nay_total=int(vote.findtext('DecisionDivisionNumberOfNays')),
             paired_total=int(vote.findtext('DecisionDivisionNumberOfPaired')))
@@ -73,23 +129,26 @@ def import_votes():
         except Exception:
             logger.exception("Couldn't get french description for vote %s" % votenumber)
 
-        # Okay, save the question, start processing members.
         votequestion.save()
 
-        detailurl = VOTEDETAIL_URL.format(parliamentnum=session.parliamentnum,
-            sessnum=session.sessnum, votenumber=votenumber)
+        detailurl = VOTEDETAIL_URL.format(
+            parliamentnum=session.parliamentnum,
+            sessnum=session.sessnum,
+            votenumber=votenumber,
+        )
         resp = requests.get(detailurl)
         resp.raise_for_status()
         detailroot = etree.fromstring(resp.content)
 
         for voter in detailroot.findall('VoteParticipant'):
-            pol = Politician.objects.get_by_parl_mp_id(voter.find('PersonId').text,
-                session=session, riding_name=voter.find('ConstituencyName').text)
-            # name = (voter.find('PersonOfficialFirstName').text 
-            #     + ' ' + voter.find('PersonOfficialLastName').text)
-            # riding = Riding.objects.get_by_name(voter.find('ConstituencyName').text)
-            # pol = Politician.objects.get_by_name(name=name, session=session, riding=riding)
-            member = ElectedMember.objects.get_by_pol(politician=pol, date=votequestion.date)
+            pol = Politician.objects.get_by_parl_mp_id(
+                voter.find('PersonId').text,
+                session=session,
+                riding_name=voter.find('ConstituencyName').text,
+            )
+            member = ElectedMember.objects.get_by_pol(
+                politician=pol, date=votequestion.date
+            )
             if voter.find('IsVoteYea').text == 'true':
                 ballot = 'Y'
             elif voter.find('IsVoteNay').text == 'true':
@@ -97,10 +156,46 @@ def import_votes():
             elif voter.find('IsVotePaired').text == 'true':
                 ballot = 'P'
             else:
-                raise Exception("Couldn't parse RecordedVote for %s in vote %s" % (name, votenumber))
-            MemberVote(member=member, politician=pol, votequestion=votequestion, vote=ballot).save()
+                display_name = getattr(pol, 'name', 'unknown')
+                raise Exception(
+                    "Couldn't parse RecordedVote for %s in vote %s" % (
+                        display_name,
+                        votenumber,
+                    )
+                )
+            MemberVote(
+                member=member,
+                politician=pol,
+                votequestion=votequestion,
+                vote=ballot,
+            ).save()
         votequestion.label_absent_members()
         votequestion.label_party_votes()
         for mv in votequestion.membervote_set.all():
             mv.save_activity()
+
+        if (
+            latest_timestamp is None
+            or event_dt > latest_timestamp
+            or (
+                event_dt == latest_timestamp
+                and token_meta['vote'] > latest_meta.get('vote', -1)
+            )
+        ):
+            latest_timestamp = event_dt
+            latest_token = token
+            latest_meta = token_meta
+
+    if latest_timestamp and (
+        watermark.timestamp is None
+        or latest_timestamp > watermark.timestamp
+        or latest_token != watermark.token
+    ):
+        update_watermark(
+            "votes",
+            token=latest_token,
+            timestamp=latest_timestamp,
+            metadata=latest_meta,
+        )
+
     return True
